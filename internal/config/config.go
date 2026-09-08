91526f7788048ceafc264ad913fa3ee3b18a677c
Shivam Nayak <shivamkumarnayak13@gmail.com>
2026-09-08 13:09:39 +0530

    feat: add application configuration

---
+++ internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+func String(key string, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
+}
+
+func Int(key string, fallback int) int {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+
+	parsed, err := strconv.Atoi(value)
+	if err != nil {
+		return fallback
+	}
+
+	return parsed
+}
